900ce1410f8703953ad79f0443fc3e5ac48dbb8e
Jesse Gabriel <jessetirayoh@gmail.com>
2018-09-28 16:35:57 -0700

    Added Image URL display functionality

--- src/App.js
+++ src/App.js
@@ -52,14 +52,14 @@ class App extends Component {
 	}
 
 	onInputChange = event => {
-		console.log(event.target.value);
+		this.setState({ input: event.target.value });
 	};
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, "https://samples.clarifai.com/metro-north.jpg").then(
+		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
 			function(response) {
-				console.log(response);
+				console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
 			},
 			function(err) {
 				// there was an error
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,7 +3,9 @@ import React from "react";
 const FaceRecognition = ({ imageUrl }) => {
 	return (
 		<div className="center">
-			<img src={"imageUrl"} alt="" />
+			<div className="absolute mt2">
+				<img src={imageUrl} width="500px" height="auto" alt="" />
+			</div>
 		</div>
 	);
 };
